8541df8df005e42a87affd61f2bbe83659ae7774
MJ34 <kasur.rusak404@gmail.com>
2022-05-07 23:03:20 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,24 +6,24 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>#MujiSellaUntukSelamanya</title>
-        <meta name="title" content="#DennySiskaUntukSelamanya" />
+        <meta name="title" content="#MujiSellaUntukSelamanya" />
         <meta
           name="description"
-          content="Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud menyelenggarakan pernikahan putra-putri kami: Andika Fransiskayana dan Denny Aditya Pradipta."
+          content="Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud menyelenggarakan pernikahan putra-putri kami: Sella Puspita Rani dan Mujiono."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://denny-siska.vercel.app" />
-        <meta property="og:title" content="#DennySiskaUntukSelamanya" />
+        <meta property="og:url" content="https://undanganmujisella.vercel.app" />
+        <meta property="og:title" content="#MujiSellaUntukSelamanya" />
         <meta
           property="og:description"
-          content="Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud menyelenggarakan pernikahan putra-putri kami: Andika Fransiskayana dan Denny Aditya Pradipta."
+          content="Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud menyelenggarakan pernikahan putra-putri kami: Sella Puspita Rani dan Mujiono."
         />
         <meta
           property="og:image"
           content="https://muji-sella.vercel.app/images/landing_.jpg"
         />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://muji-sella.vercel.app" />
+        <meta property="twitter:url" content="https://undanganmujisella.vercel.app" />
         <meta property="twitter:title" content="#MujiSellaUntukSelamanya" />
         <meta
           property="twitter:description"
@@ -31,7 +31,7 @@ function MyApp({ Component, pageProps }) {
         />
         <meta
           property="twitter:image"
-          content="https://denny-siska.vercel.app/images/landing_.jpg"
+          content="https://undanganmujisella.vercel.app/images/landing_.jpg"
         />
       </Head>
       <Component {...pageProps} />
